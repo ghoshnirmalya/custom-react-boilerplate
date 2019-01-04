9cfba6ecf1a7d19268d8aba43640e0b69bf355f4
Nirmalya Ghosh <nirmalya.email@gmail.com>
2019-01-04 21:20:30 +0530

    chore(package): Upgrades all packages & adds connected-react-router

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,12 @@
 import { combineReducers } from 'redux'
-import { routerReducer } from 'react-router-redux'
+import { connectRouter } from 'connected-react-router'
 
 import auth from './auth'
 import users from './users'
 
-export default combineReducers({
-  router: routerReducer,
-  auth,
-  users
-})
+export default history =>
+  combineReducers({
+    router: connectRouter(history),
+    auth,
+    users
+  })
